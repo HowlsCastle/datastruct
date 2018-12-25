e0be93ee4c906156c5eebb0ab6bdb212a5891b7e
liuyl <liuyinling@100tal.com>
2018-12-25 16:58:29 +0800

    增加java实现数组

---
+++ datastruct-server/src/main/java/com/example/datastruct/DatastructServerApplication.java
@@ -0,0 +1,12 @@
+package com.example.datastruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatastructServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatastructServerApplication.class, args);
+	}
+}
---
+++ datastruct-server/src/main/java/com/example/datastruct/array/Array.java
@@ -0,0 +1,70 @@
+package com.example.datastruct.array;
+
+public class Array {
+    //定义整型数据data保存数据
+    public int data[];
+    //定义数组长度
+    private int n;
+    //定义中实际个数
+    private int count;
+
+    public Array(int n) {
+        this.data = new int[n];
+        this.n = n;
+        this.count = 0;
+    }
+
+    public boolean insert(int position, int value) {
+        if (count == n) {
+            return false;
+        }
+
+        if (position < 0 || position > count) {
+            return false;
+        }
+
+        for(int i=count; i>position;i--) {
+            data[count] = data[count-1];
+        }
+        data[position] = value;
+        count++;
+        return true;
+    }
+
+    public boolean delete(int index) {
+        if (index < 0 || index >= count) {
+            return false;
+        }
+        for(int i=index+1; i < count; i++) {
+            data[i-1] = data[i];
+        }
+        count--;
+        return true;
+    }
+
+    public void printAll() {
+        for (int i = 0; i < count; ++i) {
+            System.out.print(data[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Array array = new Array(5);
+        array.printAll();
+        array.insert(0, 3);
+        array.insert(0, 4);
+        array.insert(1, 5);
+        array.insert(3, 9);
+        array.insert(3, 10);
+//        array.insert(3, 11);
+        array.printAll();
+        array.delete(0);
+        array.printAll();
+        array.insert(3, 11);
+        array.printAll();
+
+    }
+
+
+}
